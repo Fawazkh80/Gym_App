3b7f6c7c6a33ad478e776bece54ea69a972b649e
Fawazkh80 <91918080+Fawazkh80@users.noreply.github.com>
2022-01-19 20:29:28 +0300

    new changes

--- Exercise/models.py
+++ Exercise/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 
 class weights(models.Model):
     exe_name = models.TextField(max_length = 100, blank=True)
-    note = models.TextField(max_length = 100, blank=True)
+    note = models.TextField(max_length = 500, blank=True)
     category = models.CharField(max_length = 100, blank=True)
     reps = models.IntegerField(null=True, blank=True)
     sets = models.IntegerField(null=True, blank=True)
